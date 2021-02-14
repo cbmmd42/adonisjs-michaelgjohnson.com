c7e2e369c127bbc6b50db27b27a9a5c76fecd701
Michael Johnson <cbmmd42@gmail.com>
2021-02-14 16:55:40 -0600

    MJ-1 fixes culp config and adds container to nav

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,12 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-var input = './resources/styles/*.scss';
+var input = './resources/assets/sass/*.scss';
 var output = './public/css';
 
 gulp.task('sass', function () {
     return gulp
-        // Find all `.scss` files from the `stylesheets/` folder
+        // Find all `.scss` files from the `styles/` folder
         .src(input)
         // Run Sass on those files
         .pipe(sass())
