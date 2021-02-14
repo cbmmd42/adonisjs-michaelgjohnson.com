de113054f8b4f4181b71e17f3822d765ac561211
Michael Johnson <cbmmd42@gmail.com>
2021-02-14 00:34:36 -0600

    MJ-1 sets up layout and simple scss configuration

---
+++ Gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+var input = './resources/styles/*.scss';
+var output = './public/css';
+
+gulp.task('sass', function () {
+    return gulp
+        // Find all `.scss` files from the `stylesheets/` folder
+        .src(input)
+        // Run Sass on those files
+        .pipe(sass())
+        // Write the resulting CSS in the output folder
+        .pipe(gulp.dest(output));
+});
+
+var sassOptions = {
+    errLogToConsole: true,
+    outputStyle: 'expanded'
+};
+
+gulp.task('sass', function () {
+    return gulp
+        .src(input)
+        .pipe(sass(sassOptions).on('error', sass.logError))
+        .pipe(gulp.dest(output));
+});
